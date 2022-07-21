aa3d00cf3a255dc791c0912d4e910c5c44e29aa9
Vernon Swanepoel <frantic.naturalist@gmail.com>
2022-07-21 09:03:16 +0100

    Slight refactor, and more work on auth

--- backend/app/main.py
+++
@@ -1,7 +0,0 @@
-import datetime
-from flask import Flask, jsonify, request, make_response
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_object("app.config.Config")
-db = SQLAlchemy(app)
